bb9dee7c3a3d42d4f61c1381396af6f1398da2be
leo-chernyak <leoev.chernyak@gmail.com>
2025-12-20 00:10:10 +0100

    fuck

--- miniapp/vite.config.ts
+++ miniapp/vite.config.ts
@@ -3,21 +3,25 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+
+  // GitHub Pages base path
+  // https://leo-chernyak.github.io/telegram-mini-app/
+  base: '/telegram-mini-app/',
+
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
   },
+
   server: {
     host: '127.0.0.1',
     port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  }
+        changeOrigin: true,
+      },
+    },
+  },
 });
-
-
